d4ec0689e96f5b4e196e88552c11981b269d1d84
shura <shura1991@gmail.com>
2010-11-15 10:40:20 +0100

    Added no-ip.com service

---
+++ lib/dyndyndong/services/no-ip.rb
@@ -0,0 +1,47 @@
+#--
+# Copyleft shura. [shura1991@gmail.com]
+#
+# This file is part of DynDynDong.
+#
+# DynDynDong is free software: you can redistribute it and/or modify 
+# it under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# DynDynDong is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with dyndyndong. If not, see <http://www.gnu.org/licenses/>.
+#++
+
+module DynDynDong
+
+class NoIp < DynDNS
+  def MSGTABLE(x)
+    {
+      'good'      => 'DNS hostname update successful.',
+      'nochg'     => 'IP address is current, no update performed.',
+      'nohost'    => 'Hostname supplied does not exist under specified account.',
+      'badauth'   => 'Invalid username password combination.',
+      'badagent'  => 'Client disabled.',
+      '!donator'  => 'An update request was sent including a feature that is not available to that particular user such as offline options.',
+      'abuse'     => 'Username is blocked due to abuse.',
+      '911'       => 'A fatal error on our side such as a database outage.'
+    }
+  end
+
+private
+
+  def update_host
+    'dynupdate.no-ip.com'
+  end
+
+  def offline?(offline)
+    super(offline).gsub(/CHG$/, '')
+  end
+end
+
+end
